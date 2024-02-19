515b45b925597d9903223e76070610a03514c9ba
Carlos Chongo <cacc.chongo@gmail.com>
2024-02-19 13:25:48 -0600

    typo: agregando funciones y descripcion del ejercicio

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hola Ch38");
+
+function EnviandoSaludos(){
+    alert('Hola Ch38');
+}
+/**
+ * pedir el nommbre de la persona usando promt()
+ * Mostrar un saludo y nombre de la persona con alert()
+ */
+function enviandoSaludosPersona(ingresoNombre){
+    let nombre = prompt("Ingrese su nombre");
+    alert(`Saludos ${nombre}, un gusto conocerlo` )
+}
